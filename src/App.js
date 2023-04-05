522afe2199c95e9e0088c19b5d5b964643eb5884
Ojhal Patwa <ojhal.patwa.taskmonk.ai>
2023-04-05 20:37:09 +0530

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
+import React, { useState } from 'react';
 
 function App() {
+  const [mode,setMode] = useState('dark')
+
+  const toggleMode = () => {
+    console.log("toggled")
+    if(mode === "light"){
+      setMode("dark");
+      document.body.style.backgroundColor="white"
+    }
+    else{
+      setMode("light");
+      document.body.style.backgroundColor="black"
+    }
+  }
   return (
     <>
-    <Navbar title="TextUtils"/>
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
     <div className='container my-3'>
       <TextForm heading="Enter the Text to analyze below"/>
     </div>
